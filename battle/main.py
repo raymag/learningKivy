655cbee91111e4ac8806cbdaae799bb557e94668
raymag <prayk11@outlook.com>
2017-11-08 11:40:49 -0200

    treino 06

--- battle/main.py
+++ battle/main.py
@@ -9,48 +9,72 @@ import json
 
 class Battle(BoxLayout):
       def __init__(self, **kwargs):
-            super(Battle, self).__init__(**kwargs, orientation='vertical')
-            def loadData(self): #Carrega os Dados do Programa
-                  global database
-                  database = ['Jucelino']
+            super(Battle, self).__init__(orientation='vertical')
+            def newGame(self):
+                global database
+                database = ['Jucelino', 0, 10, 2, 100, 10, 2, 0, 100]
+                print(self.parent.parent.child)
+                loadStats(self, database)
+            def loadStats(self, database):
+                  self.nickName = database[0]
+                  self.score = database[1]
+                  self.playerHp = database[2]
+                  self.playerShield = database[3]
+                  self.playerCoin = database[4]
+                  self.enemyHp = database[5]
+                  self.enemyShield = database[6]
+                  self.playerBottle = database[7]
+                  self.enemyCoin = database[8]
+                  print(self.nickName)
+            def loadData(self, database): #Carrega os Dados do Programa
                   try:
                         with open('data/database.json', 'r') as db:
                               database = json.load(db)
-                  except FileNotFoundError:
+                  except:
                         with open('data/database.json', 'w') as db:
                               database = json.dump(database, db)
             def loadMainLayout(self): #Carrega o Layout Inicial
-                  #Seções Raízes ==================
+                  #Secoes Raizes ==================
                   topBox = BoxLayout()
                   bottomBox = BoxLayout(orientation='vertical')
-                  topper = BoxLayout()
+                  topper = GridLayout(cols=2)
 
                   self.add_widget(topper)
                   self.add_widget(topBox)
                   self.add_widget(bottomBox)
 
-                  #Seções topBox ==================
+                  #Secoes topBox ==================
                   leftGrid = GridLayout(cols=2)
                   rightGrid = GridLayout(cols=2)
 
                   topBox.add_widget(leftGrid)
                   topBox.add_widget(rightGrid)
 
-                  #Seções Topper =================
+                  #Secoes Topper =================
                   nameLabelText = Label(text='Nome:')
-                  nameLabelValue = Label(text=str(database[0]))
-                  
+                  nameLabelValue = Label(text=self.nickName)
+                  scoreLabelText = Label(text='Score:')
+                  scoreLabelValue = Label(text=str(self.score))
+                  playerSide = Label(text='== Jogador ==')
+                  enemySide = Label(text='== Oponente ==')
+
                   topper.add_widget(nameLabelText)
                   topper.add_widget(nameLabelValue)
-                  
+                  topper.add_widget(scoreLabelText)
+                  topper.add_widget(scoreLabelValue)
+                  topper.add_widget(playerSide)
+                  topper.add_widget(enemySide)
 
-                  #Seções leftGrid =================
+
+                  #Secoes leftGrid =================
                   hpPlayerLabelText = Label(text='Vida:')
-                  hpPlayerLabelValue = Label(text='10')
+                  hpPlayerLabelValue = Label(text=str(self.playerHp))
                   coinPlayerLabelText = Label(text='Moedas:')
-                  coinPlayerLabelValue = Label(text='000')
-                  hpotPlayerLabelText = Label(text='HP Pot')
-                  hpotPlayerLabelValue = Label(text='00')
+                  coinPlayerLabelValue = Label(text=str(self.playerCoin))
+                  hpotPlayerLabelText = Label(text='HP Bottle:')
+                  hpotPlayerLabelValue = Label(text=str(self.playerBottle))
+                  shieldPlayerLabelText = Label(text='Escudo:')
+                  shieldPlayerLabelValue = Label(text=str(self.playerShield))
 
                   leftGrid.add_widget(hpPlayerLabelText)
                   leftGrid.add_widget(hpPlayerLabelValue)
@@ -58,23 +82,43 @@ class Battle(BoxLayout):
                   leftGrid.add_widget(coinPlayerLabelValue)
                   leftGrid.add_widget(hpotPlayerLabelText)
                   leftGrid.add_widget(hpotPlayerLabelValue)
+                  leftGrid.add_widget(shieldPlayerLabelText)
+                  leftGrid.add_widget(shieldPlayerLabelValue)
 
-                  #Seções rightGrid ================
+                  #Secoes rightGrid ================
                   hpEnemyLabelText = Label(text='Vida:')
-                  hpEnemyLabelValue = Label(text='10')
-      
+                  hpEnemyLabelValue = Label(text=str(self.enemyHp))
+                  coinEnemyLabelText = Label(text='Moedas:')
+                  coinEnemyLabelValue = Label(text=str(self.enemyCoin))
+                  shieldEnemyLabelText = Label(text='Escudo:')
+                  shieldEnemyLabelValue = Label(text=str(self.enemyShield))
+
                   rightGrid.add_widget(hpEnemyLabelText)
                   rightGrid.add_widget(hpEnemyLabelValue)
+                  rightGrid.add_widget(coinEnemyLabelText)
+                  rightGrid.add_widget(coinEnemyLabelValue)
+                  rightGrid.add_widget(shieldEnemyLabelText)
+                  rightGrid.add_widget(shieldEnemyLabelValue)
 
-                  #Seções Bottom ================
+                  #Secoes Bottom ================
                   atackButton = Button(text='Atacar')
+                  buyHpotButton = Button(text='Comprar HP Bottle')
+                  buyShieldButton = Button(text='Comprar Escudo')
+                  saveButton = Button(text='Salvar')
+                  newGameButton =Button(text='Novo Jogo', on_release=newGame)
 
                   bottomBox.add_widget(atackButton)
-            
-            loadData(self)
+                  bottomBox.add_widget(buyHpotButton)
+                  bottomBox.add_widget(buyShieldButton)
+                  bottomBox.add_widget(saveButton)
+                  bottomBox.add_widget(newGameButton)
+            database = ['Jucelino', 0, 10, 2, 100, 10, 2, 0, 100]
+            loadData(self, database)
+            loadStats(self, database)
             loadMainLayout(self)
-            
-      
+            print(self.nickName)
+
+
 
 class App(App):
       def build(self):
